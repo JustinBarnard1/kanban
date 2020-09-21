199683c6d3bec125ae9a874c155cb090b9ef3086
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-21 14:10:53 -0600

    fixed auth bug

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// @ts-ignore
 import Home from '../views/Home.vue'
+// @ts-ignore
 import Boards from '../views/Boards.vue'
+// @ts-ignore
 import Board from '../views/Board.vue'
+// @ts-ignore
 import { authGuard } from "@bcwdev/auth0-vue"
 
 Vue.use(Router)
@@ -23,7 +27,8 @@ export default new Router({
     {
       path: '/boards/:boardId',
       name: 'board',
-      component: Board
+      component: Board,
+      beforeEnter: authGuard
     },
     {
       path: "*",
