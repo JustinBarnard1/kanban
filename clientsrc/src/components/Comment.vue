598c8a74f463afb0c3f0df20fa92453675099ccc
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-22 16:56:56 -0600

    More styling board vue

--- clientsrc/src/components/Comment.vue
+++ clientsrc/src/components/Comment.vue
@@ -1,46 +1,52 @@
 <template>
-    <div class="">
-        <li class="card">
-        <p v-if="!editing" @click="editBody" >{{commentProp.body}}</p>
-        <form  @submit.prevent="editFinish" v-if="editing" class="form-inline">
-          <div class="form-group">
-            <input  v-model="comment.body" type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-          </div>
-        <button  type="submit" class="btn btn-success">Done</button>
-        </form>
-        <small><i @click="deleteComment">Delete Comment</i></small>
-        </li>
-    </div>  
+  <div class="mx-2 mb-2">
+    <li class="card">
+      <p v-if="!editing" @click="editBody">{{commentProp.body}}</p>
+      <form @submit.prevent="editFinish" v-if="editing" class="form-inline">
+        <div class="form-group">
+          <input
+            v-model="comment.body"
+            type="text"
+            name
+            id
+            class="form-control"
+            placeholder
+            aria-describedby="helpId"
+          />
+        </div>
+        <button type="submit" class="btn btn-success">Done</button>
+      </form>
+      <small>
+        <i class @click="deleteComment">Delete Comment</i>
+      </small>
+    </li>
+  </div>
 </template>
 
 <script>
- 
 export default {
-props: ["commentProp"],
-data(){
-  return {
-    editing: false,
-    comment: {},
-  }
-},
-methods: {
-  deleteComment(){
-    this.$store.dispatch('deleteComment', this.commentProp)
+  props: ["commentProp"],
+  data() {
+    return {
+      editing: false,
+      comment: {},
+    };
   },
-  editBody(){
-    this.editing = true
-    this.comment = this.commentProp
+  methods: {
+    deleteComment() {
+      this.$store.dispatch("deleteComment", this.commentProp);
+    },
+    editBody() {
+      this.editing = true;
+      this.comment = this.commentProp;
+    },
+    editFinish() {
+      this.$store.dispatch("editComment", this.comment);
+      this.editing = false;
+    },
   },
-  editFinish(){
-    this.$store.dispatch('editComment', this.comment)
-    this.editing = false
-  },
-},
-
-}
-
+};
 </script>
 
 <style>
-
 </style>
