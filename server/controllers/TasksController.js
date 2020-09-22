5eccb275976a29d708dee1c6e2fa6dd8d7af5fd1
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-22 10:23:10 -0600

    task backend complete

--- server/controllers/TasksController.js
+++ server/controllers/TasksController.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import BaseController from "../utils/BaseController";
 import auth0provider from "@bcwdev/auth0provider";
-import { listService } from '../services/ListService'
 import { taskService } from '../services/TaskService'
 
 
 
 //PUBLIC
-export class ListsController extends BaseController {
+export class TasksController extends BaseController {
   constructor() {
-    super("api/lists")
+    super("api/tasks")
     this.router
       .use(auth0provider.getAuthorizedUserInfo)
-      .get('/:id/tasks', this.getAll)
+      .get('', this.getAll)
       .post('', this.create)
       .put('/:id', this.edit)
       .delete('/:id', this.delete)
@@ -21,38 +20,30 @@ export class ListsController extends BaseController {
 
   async getAll(req, res, next) {
     try {
-      //only gets lists by user who is logged in
+      //only gets tasks by user who is logged in
       let data = await taskService.getAll(req.params.id)
       return res.send(data)
     }
     catch (err) { next(err) }
   }
-
-  async getById(req, res, next) {
-    try {
-      let data = await listService.getById(req.params.id, req.userInfo.email)
-      return res.send(data)
-    } catch (error) { next(error) }
-  }
-
   async create(req, res, next) {
     try {
       req.body.creatorEmail = req.userInfo.email
-      let data = await listService.create(req.body)
+      let data = await taskService.create(req.body)
       return res.status(201).send(data)
     } catch (error) { next(error) }
   }
 
   async edit(req, res, next) {
     try {
-      let data = await listService.edit(req.params.id, req.userInfo.email, req.body)
+      let data = await taskService.edit(req.params.id, req.userInfo.email, req.body)
       return res.send(data)
     } catch (error) { next(error) }
   }
 
   async delete(req, res, next) {
     try {
-      await listService.delete(req.params.id, req.userInfo.email)
+      await taskService.delete(req.params.id, req.userInfo.email)
       return res.send("Successfully deleted")
     } catch (error) { next(error) }
   }
